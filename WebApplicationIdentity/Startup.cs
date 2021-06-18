d481b3a1243d539b1bd32b0a93408d05ad4e3f4c
Sam.Tek <sonny.foot@hotmail.com>
2021-06-18 13:21:19 +0200

    define our user Identity with a class heritage

--- WebApplicationIdentity/Startup.cs
+++ WebApplicationIdentity/Startup.cs
@@ -12,6 +12,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebApplicationIdentity.Data;
+using WebApplicationIdentity.Models;
 
 namespace WebApplicationIdentity
 {
@@ -32,7 +33,7 @@ namespace WebApplicationIdentity
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDatabaseDeveloperPageExceptionFilter();
 
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
         }
