06721877112ea1eb5d985099f4135901530bf34a
Sam.Tek <sonny.foot@hotmail.com>
2021-06-18 13:07:29 +0200

    Edit Name BDD and add migration and updatedatabase

---
+++ WebApplicationIdentity/Data/Migrations/20210618105751_Int.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplicationIdentity.Data.Migrations
+{
+    public partial class Int : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
